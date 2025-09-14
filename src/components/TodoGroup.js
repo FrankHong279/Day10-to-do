06ded44ae42258e1dd4c92145d48928f19e95760
HONGFR <HONGFR@OOCL.COM>
2025-09-14 10:09:27 +0800

    feat：show message when no todo list

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -8,6 +8,11 @@ export function TodoGroup() {
     const {state, dispatch} = useContext(TodoContext)
     return <div className={"todo-group"}>
         <h1>Todo List</h1>
+        {state.length === 0 && (
+            <div style={{marginBottom: "1em", marginLeft: "2em"}}>
+                add the things you need to do today...
+            </div>
+        )}
         {
             state.map((item, index) => {
                 return <TodoItem todo={item} key={index}/>

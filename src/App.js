79c773e4b4bfcc2dda5131ed84d20c5d1b017a4c
HONGFR <HONGFR@OOCL.COM>
2025-09-15 10:32:28 +0800

    feat: create browser router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import {TodoList} from "./components/TodoList";
+import {createBrowserRouter} from "react-router";
 
 export const initState = [
     {id: 1, text: "the first todo", done: false},
     {id: 2, text: "the second todo", done: true},
 ];
+
+createBrowserRouter([
+    {
+        path: "/",
+        element: <TodoList/>,
+    }
+]);
 
 function App() {
     return (

4d91099263ec0fe6bc840998d4a623f9f509cf0c
HONGFR <HONGFR@OOCL.COM>
2025-09-15 14:11:45 +0800

    refactor: remove unused component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,5 @@
-import {useReducer} from "react";
-import {todoReducer} from "../reducers/TodoReducer";
-import {TodoContext as TodoContext1} from "../contexts/TodoContext";
 import {TodoGroup} from "./TodoGroup";
 import {TodoGenerator} from "./TodoGenerator";
-import {initState} from "../App";
 
 export function TodoList() {
     return (

bf1f79c4bb472186ca6a107173fe9c3f36adf961
HONGFR <HONGFR@OOCL.COM>
2025-09-16 10:24:44 +0800

    feat: add 404 axios handler

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -1,9 +1,26 @@
 import axios from "axios";
+import {message} from "antd";
 
-export const api = axios.create({
-    baseURL: "https://68c7ac8c5d8d9f514732871a.mockapi.io/",
+const api = axios.create({
+    baseURL: "https://68c7ac8c5d8d9f514732871a.mockapi.io",
     headers: {
         "Content-Type": "application/json"
     },
     timeout: 10_000
 })
+
+api.interceptors.response.use(
+    (response) => {
+        // handle response
+        return response;
+    },
+    (error) => {
+        const {status} = error.response;
+        if (status === 404) {
+            message.error(error.message, 10_000).then(r => {});
+        }
+        return Promise.reject(error);
+    }
+);
+
+export {api}
